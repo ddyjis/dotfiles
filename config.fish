4dfffab2849871f4239f199c9e0596d83ac6acd7
Takusei Ri <ddyjis@Takuseis-MacBook-Pro.local>
2020-05-03 22:11:14 -0400

    Add script for auto-start Tmux

--- config.fish
+++ config.fish
@@ -4,3 +4,9 @@ set LC_TIME ja_JP
 # !! Contents within this block are managed by 'conda init' !!
 eval /usr/local/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
+
+# Auto-start Tmux
+if status is-interactive
+and not set -q TMUX
+    exec tmux
+end
