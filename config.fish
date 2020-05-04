6abc870f8c49a55af81496121370d8e1a7db69b6
Takusei Ri <ddyjis@Takuseis-MacBook-Pro.local>
2020-05-03 22:09:00 -0400

    Add conda init to fish

--- config.fish
+++ config.fish
@@ -1,1 +1,6 @@
 set LC_TIME ja_JP
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /usr/local/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
