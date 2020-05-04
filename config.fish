0356efa68e07c1ce67f75deccd8bfb6a029b6cf5
ddyjis <ddyjis@gmail.com>
2020-05-04 01:28:00 -0400

    Override conda prompt

--- config.fish
+++ config.fish
@@ -4,6 +4,8 @@ set LC_TIME ja_JP
 # !! Contents within this block are managed by 'conda init' !!
 eval /usr/local/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
+# Override conda prompt
+function __conda_add_prompt; end
 
 # Auto-start Tmux
 if status is-interactive
