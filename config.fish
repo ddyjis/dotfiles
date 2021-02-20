714ff0b217422f8ca697a3addd192d21fcce345c
ddyjis <ddyjis@gmail.com>
2021-02-19 23:06:30 -0500

    Add zelda theme fish prompt

--- config.fish
+++ config.fish
@@ -9,7 +9,20 @@ and not set -q TMUX
     exec tmux
 end
 
-thefuck --alias | source
+# Fish welcome message
+function fish_greeting
+    echo -e "IT IS DANGEROUS TO GO ALONE! TAKE THIS. ✝"
+end
 
-# Add Go Path
-set PATH $PATH /Users/ddyjis/go/bin
+function fish_prompt
+    set -l triforce_color d4ce46
+    set -l botw_tunic 38b6f1
+
+    set -g __fish_git_prompt_show_informative_status true
+    set -g __fish_git_prompt_showdirtystate true
+    set -g __fish_git_prompt_showuntrackedfiles true
+    set -g __fish_git_prompt_showstashstate true
+    set_color $botw_tunic; echo "  " (prompt_pwd) (fish_git_prompt)
+    set_color normal
+    set_color $triforce_color; echo -n " ▲ "
+end
