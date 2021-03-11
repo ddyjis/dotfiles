9f45ef62dc4bbd66e7ca68617a90e6f6c4f591f1
ddyjis <ddyjis@gmail.com>
2021-03-11 00:08:35 -0500

    Resume tmux session when fish startup

--- config.fish
+++ config.fish
@@ -6,7 +6,7 @@ function __conda_add_prompt; end
 # Auto-start Tmux
 if status is-interactive
 and not set -q TMUX
-    exec tmux
+    exec tmux attach -t 0 || tmux new
 end
 
 # Fish welcome message
