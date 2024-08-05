fe3e76496f90ae2b9e22a949c391212f991e9fc1
ddyjis <ddyjis@gmail.com>
2024-08-05 15:45:16 -0400

    feat(neovim): format on save

--- private_dot_config/nvim/lua/plugins/lsp-config.lua
+++ private_dot_config/nvim/lua/plugins/lsp-config.lua
@@ -70,6 +70,7 @@ return {
 		},
 		config = function()
 			local null_ls = require("null-ls")
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 			null_ls.setup({
 				sources = {
 					null_ls.builtins.formatting.stylua,
@@ -77,6 +78,19 @@ return {
 					require("none-ls.diagnostics.eslint_d"),
 					require("none-ls.code_actions.eslint"),
 				},
+				-- sync formatting on save
+				on_attach = function(client, bufnr)
+					if client.supports_method("textDocument/formatting") then
+						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							group = augroup,
+							buffer = bufnr,
+							callback = function()
+								vim.lsp.buf.format({ async = false })
+							end,
+						})
+					end
+				end,
 			})
 		end,
 		keys = {
