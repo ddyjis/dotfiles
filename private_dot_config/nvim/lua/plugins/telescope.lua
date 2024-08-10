c1a9cfcb999d050cc295005dfe4976b0c77ad697
ddyjis <ddyjis@gmail.com>
2024-08-10 18:45:13 -0400

    chore(neovim): fix formatting

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -1,29 +1,29 @@
 return {
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    event = "VimEnter",
-    config = function()
-      require("telescope").setup({
-        defaults = {
-          vimgrep_arguments = { 'ag', '--nocolor', '--nogroup', '--column' },
-        },
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-      })
-      require("telescope").load_extension("ui-select")
-    end,
-    keys = {
-      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-      { "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "Find Texts" },
-      { "<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "Find Buffers" },
-      { "<leader>fh", "<cmd>Telescope help_tags<cr>",  desc = "Find Helps" },
-    },
-  },
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		event = "VimEnter",
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					vimgrep_arguments = { "ag", "--nocolor", "--nogroup", "--column" },
+				},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+		keys = {
+			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+			{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Find Texts" },
+			{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find Buffers" },
+			{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Find Helps" },
+		},
+	},
 }
