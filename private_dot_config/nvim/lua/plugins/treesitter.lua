98611c5e477cd52e8f6b2e0466f3825c9763cae9
ddyjis <ddyjis@gmail.com>
2025-01-04 16:35:28 -0500

    feat(nvim): remove nvim

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      opts.ensure_installed = { "lua", "vimdoc", "javascript", "html", "typescript", "python" }
-      return opts
-    end,
-  },
-}
