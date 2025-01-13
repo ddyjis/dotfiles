03108d7b55e5fadc1d2d09ccb03281cc9f50fa5c
ddyjis <ddyjis@gmail.com>
2025-01-13 00:48:40 -0500

    feat(nvim): update lsp config

--- private_dot_config/nvim/lua/plugins/lsp-config.lua
+++ private_dot_config/nvim/lua/plugins/lsp-config.lua
@@ -1,6 +1,14 @@
 return {
-  "williamboman/mason-lspconfig.nvim",
-  opts = {
-    ensure_installed = { "lua_ls", "pyright", "tailwindcss" },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = { "lua_ls", "pyright", "tailwindcss" },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = { "stylua", "shfmt", "lua-language-server" },
+    },
   },
 }
