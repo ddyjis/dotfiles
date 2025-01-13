b7d9aa0894ece518f278ab0b1cddb05760f97596
ddyjis <ddyjis@gmail.com>
2025-01-12 22:33:05 -0500

    feat(nvim): set up snacks

---
+++ private_dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/snacks.nvim",
+  --@type snacks.Config
+  opts = {
+    lazygit = {
+      configure = true,
+      config = {
+        gui = { nerdFontsVersion = "3" },
+      },
+    },
+  },
+}
