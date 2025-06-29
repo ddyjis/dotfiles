f0356aefb75f439a968ae7e43bb4c678790b9e9d
ddyjis <ddyjis@gmail.com>
2025-06-29 00:49:04 -0400

    feat(theme): use gruvbox theme

--- private_dot_config/nvim/lua/plugins/core.lua
+++ private_dot_config/nvim/lua/plugins/core.lua
@@ -1,6 +1,25 @@
 return {
+  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
+  {
+    "f-person/auto-dark-mode.nvim",
+    priority = 1000,
+    config = function()
+      require("auto-dark-mode").setup({
+        update_interval = 1000,
+        set_dark_mode = function()
+          vim.o.background = "dark"
+          vim.cmd("colorscheme gruvbox")
+        end,
+        set_light_mode = function()
+          vim.o.background = "light"
+          vim.cmd("colorscheme gruvbox")
+        end,
+      })
+      require("auto-dark-mode").init()
+    end,
+  },
   {
     "LazyVim/LazyVim",
-    opts = { colorscheme = "tokyonight" },
+    opts = { colorscheme = "gruvbox" },
   },
 }
